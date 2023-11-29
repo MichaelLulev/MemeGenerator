91e3bdd200bf68a2e630edafc2e0d99c35f1371c
Michael Lulev <miki1990@gmail.com>
2023-11-29 12:14:57 +0200

    Refactor meme service

--- js/services/meme.service.js
+++ js/services/meme.service.js
@@ -1,10 +1,17 @@
 'use strict'
 
-const gMemes = []
-var gCurrMeme = _createMeme()
+const DEFAULT_FONT = 'Arial'
+const DEFAULT_FONT_SIZE = 20
 
-gMemes.push(gCurrMeme)
-log(gMemes)
+const gMemes = []
+var gCurrMeme
+
+function initMemes() {
+    gCurrMeme = _createMeme()
+    log(gCurrMeme)
+    gMemes.push(gCurrMeme)
+    log(gMemes)
+}
 
 function getMemes() {
     return gMemes
@@ -22,15 +29,23 @@ function addLine() {
 
 function _createMeme(image, elements=[]) {
     const meme = {
+        type: 'meme',
         image,
         elements,
     }
     return meme
 }
 
-function _createLine(text='Line Text', pos={ x: 0, y: 0 }, selected=false) {
+function _createLine(text='Line Text',
+                     font=DEFAULT_FONT,
+                     fontSize=DEFAULT_FONT_SIZE,
+                     pos={ x: 0, y: 0 },
+                     selected=false) {
     const line = {
+        type: 'line',
         text,
+        font,
+        fontSize,
         pos,
         selected,
     }
