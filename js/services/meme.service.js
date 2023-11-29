f1d8a757bb237c5ad6b68dab410e8d6f0b6e189c
Michael Lulev <miki1990@gmail.com>
2023-11-29 11:35:06 +0200

    Add getCurrMeme() and addLine()

--- js/services/meme.service.js
+++ js/services/meme.service.js
@@ -1,9 +1,23 @@
 'use strict'
 
 const gMemes = []
+var gCurrMeme = _createMeme()
+
+gMemes.push(gCurrMeme)
+log(gMemes)
 
 function getMemes() {
     return gMemes
+}
+
+function getCurrMeme() {
+    return gCurrMeme
+}
+
+function addLine() {
+    const line = _createLine()
+    gCurrMeme.elements.push(line)
+    log(gCurrMeme)
 }
 
 function _createMeme(image, elements=[]) {
@@ -11,6 +25,7 @@ function _createMeme(image, elements=[]) {
         image,
         elements,
     }
+    return meme
 }
 
 function _createLine(text='Line Text', pos={ x: 0, y: 0 }, selected=false) {
