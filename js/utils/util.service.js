56472f3b0aa3dea0dedc621824a60ec46c7229a0
Michael Lulev <miki1990@gmail.com>
2023-11-29 12:14:24 +0200

    Add rand*() util functions

--- js/utils/util.service.js
+++ js/utils/util.service.js
@@ -4,3 +4,15 @@ function log(...args) {
     args = args.map(arg => JSON.stringify(arg, null, 4))
     console.log(...args)
 }
+
+function randIntInc(min, max) {
+    return randInt(min, max + 1)
+}
+
+function randInt(min, max) {
+    return Math.floor(rand(min, max))
+}
+
+function rand(min, max) {
+    return Math.random() * (max - min) + min
+}
