336da307b8e31cde15d8add110fccd4fda4daa8b
Michael Lulev <miki1990@gmail.com>
2023-11-30 07:50:40 +0200

    Change img src to begin with root /

--- js/gallery.controller.js
+++ js/gallery.controller.js
@@ -8,7 +8,7 @@ function renderGallery() {
     var strHtml = ''
     for (let i = 1; i <= 25; i++) {
         strHtml += `
-        <img src="../img/gallery/${i}.jpg" alt="meme-image-${i}">`
+        <img src="/img/gallery/${i}.jpg" alt="meme-image-${i}">`
     }
     $('.gallery .image-container').html(strHtml)
 }
