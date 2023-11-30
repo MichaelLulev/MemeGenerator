b30d51140e2ce5f6961c9f79d837882f1605a710
Michael Lulev <miki1990@gmail.com>
2023-11-30 11:58:58 +0200

    Add more image tags

--- js/services/gallery.service.js
+++ js/services/gallery.service.js
@@ -2,9 +2,9 @@
 
 const gImages = []
 const gSearchTagsMap = {
-    1: ['dog', 'funny', 'cute'],
-    2: ['man', 'funny', 'angry', 'pointing', 'yelling'],
-    3: ['man', 'angry', 'donald', 'trump', 'cursing', 'pointing'],
+    1: ['dog', 'funny', 'cute', 'laying', 'floor'],
+    2: ['man', 'funny', 'angry', 'pointing', 'yelling', 'face'],
+    3: ['man', 'angry', 'donald', 'trump', 'cursing', 'pointing', 'suit', 'face'],
     4: ['woman', 'happy', 'oprah', 'winfrey', 'pointing', 'yelling'],
     5: ['man', 'men', 'toys', 'toy', 'story', 'woody', 'buzz', 'happy', 'worried', 'smiling'],
     6: ['man', 'crazy', 'funny', 'hair', 'explaining', 'hands', 'smiling'],
@@ -13,6 +13,20 @@ const gSearchTagsMap = {
     9: ['man', 'men', 'kissing', 'funny', 'sport'],
     10: ['man', 'hand', 'explaining', 'serious', 'hair'],
     11: ['baby', 'funny', 'surprised'],
+    12: ['man', 'vladimir', 'putin', 'pointing', 'smug', 'suit'],
+    13: ['man', 'angry', 'pointing', 'donald', 'trump', 'suit', 'face'],
+    14: ['man', 'startrek', 'hand', 'smiling', 'happy', 'bold'],
+    15: ['dog', 'dogs', 'cute', 'licking', 'happy', 'puppy', 'puppies', 'small'],
+    16: ['baby', 'babies', 'boy', 'kids', 'funny', 'dancing', 'underwear'],
+    17: ['cat', 'cute', 'laying', 'keyboard'],
+    18: ['man', 'barack', 'obama', 'smiling', 'happy', 'teeth', 'face'],
+    19: ['baby', 'dog', 'sleeping', 'cute', 'blanket', 'bed'],
+    20: ['man', 'funny', 'pointing', 'glasses'],
+    21: ['baby', 'funny', 'serious', 'fist', 'beach'],
+    22: ['baby', 'funny', 'laughing', 'evil', 'hands'],
+    23: ['man', 'funny', 'dr', 'evil', 'bald', 'fingers', 'quotes', 'face'],
+    24: ['man', 'funny', 'willi', 'wonka', 'happy', 'smiling', 'hand'],
+    25: ['woman', 'field', 'flowers', 'mountains', 'dancing', 'hands', 'happy'],
 }
 
 function initImages() {
